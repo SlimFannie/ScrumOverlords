5d761f8b07d4c7c187408de7330fd7244a7cd14f
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2023-04-12 11:13:43 -0400

    Update create.blade.php

--- FabApp/resources/views/usagers/create.blade.php
+++ FabApp/resources/views/usagers/create.blade.php
@@ -4,7 +4,13 @@
 
 @section('contenu')
 
-                    <h3 class="fontLogo">Rejoins la Fabuleuse Application!</h3>
+<div class="container-fluid h-100 g-0" id="videoFrame">
+    <video id="videoBG" autoplay muted loop src="{{ asset('img/WithoutRobotSynthwave.mp4') }}"></video>
+    <div class="container-fluid d-flex align-items-center justify-content-center h-100 g-0 d-col">
+        <div class="row g-0 pb-2">
+            <div class="card text-white bg-card text-center p-2">
+                <div class="card-body">
+                <h3 class="fontLogo">Rejoins la Fabuleuse Application!</h3>
                     <form method="post" id="FormUsager" class="text-center">
                         @csrf
                         <label for="prenom"> Prénom </label>
@@ -12,13 +18,7 @@
                         <input type="text" id="prenom" name="prenom">
                         <br>
                         <label for="nom"> Nom </label>
-                        <br><<<<<<< HEAD
-<div class="container-fluid h-100 g-0" id="videoFrame">
-    <video id="videoBG" autoplay muted loop src="{{ asset('img/WithoutRobotSynthwave.mp4') }}"></video>
-    <div class="container-fluid d-flex align-items-center justify-content-center h-100 g-0 d-col">
-        <div class="row g-0 pb-2">
-            <div class="card text-white bg-card text-center p-2">
-                <div class="card-body">
+                        <br>
                         <input type="text" id="nom" name="nom">
                         <br>
                         <label for="courriel"> Adresse courriel DU CÉGEP* </label>
@@ -35,30 +35,6 @@
                     <p>*Tu dois absolument entrer une adresse courriel du Cégep valide.</p>
                 </div>
             </div>
-=======
-<div class="container-fluid">
-    <h1>Formulaire de création de compte</h1>
-    <div class="row">
-        <div class="col">
-           <form method="post" id="FormUsager" action="{{ route('usagers.store') }}">
-                @csrf
-                <label for="prenom"> Indiquez votre prénom </label>
-                <br>
-                <input type="text" id="prenom" name="prenom">
-                <br>
-                <label for="nom"> Indiquez votre nom </label>
-                <br>
-                <input type="text" id="nom" name="nom">
-                <br>
-                <label for="courriel"> Indiquez votre adresse courriel </label>
-                <br>
-                <input type="text" id="courriel" name="adresseCourriel">
-                <br>
-                <label for="mdp"> Indiquez votre mot de passe </label>
-                <br>
-                <button type="submit" class="btn btn-success" id="btnSubmit"> Confirmer </button>
-           </form>
->>>>>>> ef2dc54c054701b0d197a4d63c916bca2b726538
         </div>
     </div>
 </div>
