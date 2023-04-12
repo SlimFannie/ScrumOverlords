115081d014814f3c3dce3225acc029b273ff00ef
fchalifour <93669813+Infinitrix10018@users.noreply.github.com>
2023-04-12 08:49:00 -0400

    Campagnes partie1

---
+++ sql/Trigger.sql
@@ -0,0 +1,9 @@
+delimiter //
+create trigger before_insert_campagne_produits
+    before insert
+    on campagne_produits
+    for each row
+begin
+    set new.campagne_id = (select id from campagnes where actif = true);
+end //
+delimiter ;
