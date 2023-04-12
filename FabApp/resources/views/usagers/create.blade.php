c0693d1be6f0724a5cbe1e10cdfb3ed712533fb0
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2023-04-12 11:07:32 -0400

    Front-end4
    Mercredi matin
    Finition front-end XL
    Page connexion page création de compte

--- FabApp/resources/views/usagers/create.blade.php
+++ FabApp/resources/views/usagers/create.blade.php
@@ -3,30 +3,37 @@
 @section('titre', 'La Fab App')
 
 @section('contenu')
-<div class="container-fluid">
-    <h1>Formulaire de création de compte</h1>
-    <div class="row">
-        <div class="col">
-           <form method="post" id="FormUsager" >
-                @csrf
-                <label for="prenom"> Indiquez votre prénom </label>
-                <br>
-                <input type="text" id="prenom" name="prenom">
-                <br>
-                <label for="nom"> Indiquez votre nom </label>
-                <br>
-                <input type="text" id="nom" name="nom">
-                <br>
-                <label for="courriel"> Indiquez votre adresse courriel </label>
-                <br>
-                <input type="text" id="courriel" name="adresseCourriel">
-                <br>
-                <label for="mdp"> Indiquez votre mot de passe </label>
-                <br>
-                <input type="text" id="mdp" name="motDePasse">
-                <br>
-                <button type="submit" class="btn btn-success" id="btnSubmit"> Confirmer </button>
-           </form>
+<div class="container-fluid h-100 g-0" id="videoFrame">
+    <video id="videoBG" autoplay muted loop src="{{ asset('img/WithoutRobotSynthwave.mp4') }}"></video>
+    <div class="container-fluid d-flex align-items-center justify-content-center h-100 g-0 d-col">
+        <div class="row g-0 pb-2">
+            <div class="card text-white bg-card text-center p-2">
+                <div class="card-body">
+                    <h3 class="fontLogo">Rejoins la Fabuleuse Application!</h3>
+                    <form method="post" id="FormUsager" class="text-center">
+                        @csrf
+                        <label for="prenom"> Prénom </label>
+                        <br>
+                        <input type="text" id="prenom" name="prenom">
+                        <br>
+                        <label for="nom"> Nom </label>
+                        <br>
+                        <input type="text" id="nom" name="nom">
+                        <br>
+                        <label for="courriel"> Adresse courriel DU CÉGEP* </label>
+                        <br>
+                        <input type="text" id="courriel" name="adresseCourriel">
+                        <br>
+                        <label for="mdp"> Mot de passe </label>
+                        <br>
+                        <input type="text" id="mdp" name="motDePasse">
+                        <br><br>
+                        <button type="submit" class="btn bg-btn d-inline-flex align-items-center" id="btnSubmit"> Confirmer <span class="material-symbols-rounded">electric_bolt</span></button>
+                    </form>
+                    <br>
+                    <p>*Tu dois absolument entrer une adresse courriel du Cégep valide.</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
