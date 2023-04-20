f7e1f82350a66d151db2c7b598f3caef716fa24f
Grondin Alexandre <117280087+Miro974@users.noreply.github.com>
2023-04-20 15:46:37 -0400

    Mise en forme

--- FabApp/resources/views/produits/create.blade.php
+++ FabApp/resources/views/produits/create.blade.php
@@ -3,10 +3,14 @@
 @section('titre', 'La Fab App')
 
 @section('contenu')
-<<<<<<< HEAD
-<div class="container-fluid mt-5">
-    <div class="row text-center">
-        <h1>Page ajout de produits</h1>
+<div class="container-fluid h-100 g-0" id="videoFrame">
+    <video id="videoBG" autoplay muted loop src="{{ asset('img/WithoutRobotSynthwave1.mp4') }}"></video>
+    <div class="container-fluid d-flex align-items-center justify-content-center h-100 g-0 d-col">
+        <div class="row g-0 pb-2">
+            <div class="col-12">
+                <div class="card text-white bg-card" id="cardFrame">
+                    <div class="card-body">
+                    <h1>Page ajout de produits</h1>
         <form method="post" id="FormProduits" class="text-center" action="{{route('produits.store')}}">
         @csrf
             <div class="row">
@@ -30,29 +34,17 @@
                     @endforeach
                 @endif
             </div>
-            <button class="btn btn-success">Ajouter un produit</button>
+            <div class="row gx-5 pt-5">
+                <div class="col text-center ">
+                    <button class="btn btn-success">Ajouter un produit</button>
+                </div>
+            </div>
         </form>
-    </div>
-</div>
-=======
-<div class="container-fluid h-100 g-0" id="videoFrame">
-    <video id="videoBG" autoplay muted loop src="{{ asset('img/WithoutRobotSynthwave1.mp4') }}"></video>
-    <div class="container-fluid d-flex align-items-center justify-content-center h-100 g-0 d-col">
-        <div class="row g-0 pb-2">
-            <div class="col-12">
-                <div class="card text-white bg-card" id="cardFrame">
-                    <div class="card-body">
-
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row gx-5 pt-5">
-            <div class="col text-center ">
-                <a href="{{route('produits.create')}}"><button class="btn btn-success">Ajouter un produit</button></a>
-            </div>
-        </div>
+        
     </div>
 </div> 
->>>>>>> db9c61ddce634e0f165341cf24c2d08dd85de1dd
 @endsection
