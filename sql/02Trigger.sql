a3f6fa9dcd7890579a1f36bd73a7259c32e9ea34
fchaf <Infinitrix@outlook.fr>
2023-04-21 11:16:18 -0400

    un petit peu de code dans un triger pas encore initialiser.

--- sql/02Trigger.sql
+++ sql/02Trigger.sql
@@ -8,7 +8,29 @@ begin
 end //
 delimiter ;
 
+delimiter //
+create trigger after_insert_campagne_detail_produits
+    before insert
+    on campagne_detail_produits
+    for each row
+begin
+    #select nomProduit from campagne_produits where id = new.campagne_produit_id;
+    #premier if sert a savoir s'il y a le produit qui exist déjà.
+    # le deuxième if va être pour savois si la liaison précise existe,
+    # donc un not exists avec le insert into dedans.
 
+    if not exists(select id from produits where nomProduit LIKE (select nomProduit from campagne_produits where id = new.campagne_produit_id)) then
+        insert into produits(nomProduit)
+        VALUES ((select nomProduit from campagne_produits where id = new.campagne_produit_id));
+    END IF;
+
+    if not exists(select id from produit_details where new.detail_id = produit_details.detail_id) then
+        insert into produit_details(produit_id, detail_id)
+        values( 1 , new.detail_id);
+    end if;
+
+end //
+delimiter ;
 
 
 
