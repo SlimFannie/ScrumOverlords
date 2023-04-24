f92293d26c89c2798b486733ea8a9231510ed3ea
Grondin Alexandre <117280087+Miro974@users.noreply.github.com>
2023-04-24 16:38:34 -0400

    Mise liste des produits

--- FabApp/resources/views/produits/index.blade.php
+++ FabApp/resources/views/produits/index.blade.php
@@ -3,39 +3,6 @@
 @section('titre', 'La Fab App')
 
 @section('contenu')
-<<<<<<< HEAD
-<div class="container-fluid mt-5">
-    <div class="row text-center">
-    <h1>Liste des produits</h1>
-        <div class="col mt-5">
-            <form method="post" action="{{ route('produits.supprimer')}}">
-                @csrf
-                <select name="typeProduit">
-                @if(count($produits))
-                    @foreach($produits as $produit)
-                        <option value="{{$produit->nomProduit}}">{{$produit->nomProduit}}</option>
-                    @endforeach
-                @endif
-                </select>
-        </div>
-    </div>
-    <div class="row text-center">
-        
-        <div class="col-xl-6 offset-xl-3 mt-5">
-
-            
-            <button type="submit" class="btn btn-danger">Supprimer un produit</button>
-            </form>
-            <a href="{{route('produits.create')}}"><button class="btn btn-success">Créer un nouveau produit</button></a>
-            <button class="btn btn-warning">Modifier un produit</button>
-
-        </div>
-    </div>
-            
-    
-    
-</div>
-=======
 <div class="container-fluid h-100 g-0" id="videoFrame">
     <video id="videoBG" autoplay muted loop src="{{ asset('img/WithoutRobotSynthwave1.mp4') }}"></video>
     <div class="container-fluid d-flex align-items-center justify-content-center h-100 g-0 d-col">
@@ -43,7 +10,7 @@
             <div class="col-12">
                 <div class="card text-white bg-card" id="cardFrame">
                     <div class="card-body">
-                        <h3>Liste des produits</h3>
+                        <h3 class="text-center">Liste des produits</h3>
                         <br>
                         <form method="post" action="{{ route('produits.supprimer')}}">
                         @csrf
@@ -54,29 +21,43 @@
                                     <td>{{ $produit->nomProduit }}</td>
                                     <td>Nb réservations</td>
                                     <td>Nb achat</td>
-                                    <td>Taille</td>
-                                    <td>Couleurs</td>
+                                    <td><select>
+                                    @if(count($tailles))
+                                        @foreach($tailles as $taille)
+                                            <option value="{{$produit->nomProduit}}">{{$taille->detail}}</option>
+                                        @endforeach
+                                    @endif
+                                    </select></td>
+                                    <td><select>
+                                    @if(count($couleurs))
+                                        @foreach($couleurs as $couleur)
+                                            <option value="{{$produit->nomProduit}}">{{$couleur->detail}}</option>
+                                        @endforeach
+                                    @endif
+                                    </select>
+                                    <input type="checkbox">
+                                    </td>
                                 </tr>
-                            @endforeach
+                            
                         </table>
+                        @endforeach
                         @endif
                     </div>
                 </div>
             </div>
         </div>
         <div class="row gx-5 pt-5">
-            <div class="col text-center">
+            <div class="col-4 text-center">
                 <button class="btn btn-danger">Supprimer un produit</button>
             </div>
             </form>
-            <div class="col text-center ">
+            <div class="col-4 text-center ">
                 <a href="{{route('produits.create')}}"><button class="btn btn-success">Créer un nouveau produit</button></a>
             </div>
-            <div class="col text-center">
+            <div class="col-4 text-center">
                 <button class="btn btn-warning">Modifier un produit</button>
             </div>
         </div>
     </div>
 </div> 
->>>>>>> db9c61ddce634e0f165341cf24c2d08dd85de1dd
 @endsection
